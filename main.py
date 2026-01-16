e2f598d365ada1b3e37570e0d0c1a7d394f2fbe0
spsdrocks <spsdrocks06@gmail.com>
2026-01-15 17:31:17 -0800

    opening camera

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import cv2
-import mediapipe as mp
 import time
+import mediapipe as mp
 
+# from mediapipe.tasks import python
+# from mediapipe.tasks.python import vision
+
+cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
+
+if not cap.isOpened():
+    print("Failed to open webcam")
+    exit()
