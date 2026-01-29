17bc655b5a8b09ca948610419f5ed915f5ccffba
spsdrocks <spsdrocks06@gmail.com>
2026-01-28 16:37:21 -0800

    Added model and process each frame, draw circles on each landmark found

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import cv2
 import time
 import mediapipe as mp
 
+# Load model prerequisites
+baseOptions = mp.tasks.BaseOptions
+handLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
+visionRunningMode = mp.tasks.vision.RunningMode
+
+# Set up model options
+modelOptions = handLandmarkerOptions(base_options=baseOptions(model_asset_path='model/hand_landmarker.task'), running_mode=visionRunningMode.VIDEO)
+
+# Create model based on options
+handLandmarker = mp.tasks.vision.HandLandmarker
+handLandmarker = handLandmarker.create_from_options(modelOptions)
+
 # Load input video file
 inputVideo = cv2.VideoCapture("media/test_video.mp4")
 
@@ -19,6 +31,9 @@ videoHeight = int(inputVideo.get(cv2.CAP_PROP_FRAME_HEIGHT))
 # Create output video instance
 outputVideo = cv2.VideoWriter("media/output_video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), videoFps, (videoWidth, videoHeight))
 
+# Councter to keep track of current frame number
+frameNumber = 0
+
 # Loop to process frames
 while (inputVideo.isOpened()):
     # Load video frame
@@ -28,10 +43,30 @@ while (inputVideo.isOpened()):
     if not videoReturn:
         break
 
-    
+    # Convert frame to model format
+    currentFrameRGB = cv2.cvtColor(videoFrame, cv2.COLOR_BGR2RGB)
+    currentFrameMP = mp.Image(image_format=mp.ImageFormat.SRGB, data=currentFrameRGB)
+
+    # Calculate frame timestamp
+    frameTimestamp = int((frameNumber / videoFps) * 1000)
+
+    # Use model on current frame
+    modelResults = handLandmarker.detect_for_video(currentFrameMP, frameTimestamp)
+
+    # Check if model found hand landmarks
+    if modelResults.hand_landmarks:
+        for hand_landmarks in modelResults.hand_landmarks:
+            for landmark in hand_landmarks:
+                # Place circles on each identified landmark
+                x = int(landmark.x * videoFrame.shape[1])
+                y = int(landmark.y * videoFrame.shape[0])
+                cv2.circle(videoFrame, (x, y), 4, (0, 255, 0), -1)
     
     # Write the processed frame to output video
     outputVideo.write(videoFrame)
+
+    # Increment frame number    
+    frameNumber += 1
 
 # Release capturer and writer
 inputVideo.release()
